6c3d0e29ce9d02098377abdcac2aae845f9e1605
Kirill <kornienko.krll@gmail.com>
2026-06-14 04:51:33 +0400

    Установка соединения с базой данных

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "KataAcademy";
+
+    public static void connectToDatabase() {
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            if (!connection.isClosed()) {
+                System.out.println("Соединение установлено");
+            }
+            } catch (SQLException e) {
+            System.err.println("Ошибка подключения");
+        }
+    }
 }
