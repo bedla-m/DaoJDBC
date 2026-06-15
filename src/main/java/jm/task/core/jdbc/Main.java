0e1569671643fe0704fc55da9311ede00c8878f9
Kirill <kornienko.krll@gmail.com>
2026-06-15 20:27:45 +0400

    update main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,17 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Джими","Хендрикс",(byte)27 );
+        userDaoJDBC.saveUser("Курт","Кобейн",(byte)27);
+        userDaoJDBC.saveUser("Дэвид", "Гилмор",(byte) 80);
+        userDaoJDBC.saveUser("Ангус","Янг",(byte)25);
+        System.out.println(userDaoJDBC.getAllUsers().toString());
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 }
