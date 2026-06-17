092adb33d4220f157b0969fdedaa1fc6666f2cce
Kirill <kornienko.krll@gmail.com>
2026-06-18 01:55:57 +0400

    style: apply code formatting and optimize imports

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -21,12 +21,12 @@ public class UserDaoHibernateImpl implements UserDao {
                 "last_Name VARCHAR(100) NOT NULL, " +
                 "age TINYINT NOT NULL" +
                 ")";
-        try(Session session = Util.getSessionFactory().openSession()){
+        try (Session session = Util.getSessionFactory().openSession()) {
             Transaction tr = session.beginTransaction();
             session.createNativeQuery(sql).executeUpdate();
             tr.commit();
             System.out.println("table create");
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Ошибка! ");
         }
     }
@@ -34,12 +34,12 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         String sql = "DROP TABLE IF EXISTS user";
-        try(Session session = Util.getSessionFactory().openSession()){
+        try (Session session = Util.getSessionFactory().openSession()) {
             Transaction tr = session.beginTransaction();
             session.createNativeQuery(sql).executeUpdate();
             tr.commit();
             System.out.println("Таблица удалена!");
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Ошибка: " + e.getMessage());
         }
     }
@@ -47,9 +47,9 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
-        try(Session session = Util.getSessionFactory().openSession()){
+        try (Session session = Util.getSessionFactory().openSession()) {
             Transaction tr = session.beginTransaction();
-            User user = new User(name,lastName,age);
+            User user = new User(name, lastName, age);
             session.save(user);
             tr.commit();
         } catch (Exception e) {
@@ -68,12 +68,13 @@ public class UserDaoHibernateImpl implements UserDao {
             System.out.println("Ошибка: " + e.getMessage());
         }
     }
+
     @Override
     public List<User> getAllUsers() {
         List<User> user = new ArrayList<>();
-        try(Session session = Util.getSessionFactory().openSession()){
-            user = session.createQuery("FROM User",User.class).list();
-        } catch (Exception e){
+        try (Session session = Util.getSessionFactory().openSession()) {
+            user = session.createQuery("FROM User", User.class).list();
+        } catch (Exception e) {
             System.out.println("Ошибка! " + e.getMessage());
         }
         return user;
@@ -81,11 +82,11 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        try(Session session = Util.getSessionFactory().openSession()){
+        try (Session session = Util.getSessionFactory().openSession()) {
             Transaction tr = session.beginTransaction();
             session.createQuery("DELETE FROM User").executeUpdate();
             tr.commit();
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Ошибка: " + e.getMessage());
         }
     }
