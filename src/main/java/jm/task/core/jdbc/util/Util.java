eec2daa9a745470f9582b964b9284eaa0130b094
Kirill <kornienko.krll@gmail.com>
2026-06-17 19:27:38 +0400

    fix: configuration

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,8 +8,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static java.lang.System.setProperty;
-
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String USERNAME = "root";
@@ -19,20 +17,23 @@ public class Util {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
+
     public static SessionFactory buildSessionFactory() {
         Configuration configuration = new Configuration();
 
         configuration
-        .setProperty("hibernate.connection.driver_class", "org.mySql.Driver")
-        .setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
-        .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/user")
-        .setProperty("hibernate.connection.username", "root")
-        .setProperty("hibernate.connection.password", "KataAcademy")
-        .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
-        .addAnnotatedClass(User.class);
+                .setProperty("hibernate.connection.driver_class", "org.mySql.Driver")
+                .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydb")
+                .setProperty("hibernate.connection.username", "root")
+                .setProperty("hibernate.connection.password", "KataAcademy")
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect")
+                .setProperty("hibernate.hbm2ddl.auto", "update")
+                .addAnnotatedClass(User.class);
         return configuration.buildSessionFactory();
     }
-    public static SessionFactory getSessionFactory(){
+
+    public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
 }
