c2ec4c82377e6fb5f854b0910aba5f7b65bdfd10
Kirill <kornienko.krll@gmail.com>
2026-06-17 18:19:31 +0400

    feat: add Hibernate configuration

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,16 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import static java.lang.System.setProperty;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "KataAcademy";
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        //test test test
+    }
+    public static SessionFactory buildSessionFactory() {
+        Configuration configuration = new Configuration();
+
+        configuration
+        .setProperty("hibernate.connection.driver_class", "org.mySql.Driver")
+        .setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
+        .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/user")
+        .setProperty("hibernate.connection.username", "root")
+        .setProperty("hibernate.connection.password", "KataAcademy")
+        .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+        .addAnnotatedClass(User.class);
+        return configuration.buildSessionFactory();
+    }
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
     }
 }
